df3ae4706e16951a01dc51f97336de5e20fbeb0f
zhinian <1604199592@qq.com>
2021-07-06 16:24:16 +0800

    first

---
+++ pinda/pd-apps/src/main/java/com/itheima/pdapps/PdAppsApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.pdapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdAppsApplication.class, args);
+    }
+
+}
---
+++ pinda/pd-apps/src/test/java/com/itheima/pdapps/PdAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.pdapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
